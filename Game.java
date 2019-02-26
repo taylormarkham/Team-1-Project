410b9f61d24f9a7a766f7c2d36f6a9db7b7311fe
jamesliu2525 <47370321+jamesliu2525@users.noreply.github.com>
2019-02-25 18:43:51 -0700

    Add files via upload

--- Game.java
+++ Game.java
@@ -21,7 +21,9 @@ public class Game {
 		GameConfig config = new GameConfig();
 		int counter = 0;
 		Scanner keyboard = new Scanner(System.in);
-		System.out.print("This is a 5x5 chess board. Player 1 controls the White pieces and Player 2 controls the Black pieces. The pieces are White Knight(wK), White Pawn(wP), Black Knight(bK), Black Pawn(bP).\n");	
+		System.out.print("This is a 5x5 chess board. Player 1 controls the White pieces and Player 2 controls the Black pieces. The pieces are White Knight(wK), White Pawn(wP), Black Knight(bK), Black Pawn(bP).\n");
+
+		//prints out what the initial board looks like	
   		for (int p = 0; p < 5; p++) {
     			for (int k = 0; k < 5; k++) {
 				System.out.print(config.getBoard()[p][k] + " ");
@@ -37,7 +39,7 @@ public class Game {
 			//keeps track of what turn it is
 			System.out.print("Turn " + counter + "\n");
 	
-			//prompts user to choose a piece
+			//prompts player 1 to choose a piece
 			System.out.print("Player 1. Choose a piece: ");
 			String change = keyboard.next();
 			while(!change.equals("wK") && !change.equals("wP")){
@@ -45,6 +47,7 @@ public class Game {
 				change = keyboard.next();
 			}
 
+			//prompts player 1 to choose the row that their piece is on from 0-4
 			System.out.print("Which row is this piece on? ");
 			int rRemove = keyboard.nextInt();
     			while (rRemove > 4 || rRemove < 0) {
@@ -52,6 +55,7 @@ public class Game {
 				rRemove = keyboard.nextInt();
 			}
 
+			//prompts player 1 to choose the column that their piece is on from 0-4
 			System.out.print("Which column is this piece on? ");
 			int cRemove = keyboard.nextInt();
     			while (cRemove > 4 || cRemove < 0) {
@@ -59,6 +63,7 @@ public class Game {
 				cRemove = keyboard.nextInt();
 			}
 
+			//checks if the piece exists at the spot on the board chosen by player 1
 			while (!config.getBoard()[rRemove][cRemove].equals(change)) {
 				System.out.print("That spot on the board does not have that piece. Pick a location where your piece exists. \n");
 				System.out.print("Which row is this piece on? ");
@@ -75,7 +80,7 @@ public class Game {
 				}
 			}
 			
-			//prompts user to move their piece to a row from 0-4
+			//prompts player 1 to move their piece to a row from 0-4
 			System.out.print("Pick a row to move your piece to: ");
 			int i = keyboard.nextInt();
     			while (i > 4 || i < 0) {
@@ -83,7 +88,7 @@ public class Game {
 				i = keyboard.nextInt();
 			}
 
-			//prompts user to move their piece to a column from 0-4
+			//prompts player 1 to move their piece to a column from 0-4
 			System.out.print("Pick a column to move your piece to: ");
 			int j = keyboard.nextInt();
     			while (j > 4 || j < 0) {
@@ -91,6 +96,7 @@ public class Game {
 				j = keyboard.nextInt();
 			}
 
+			//checks if the spot on the board that player 1 wants to move the piece to is already occupied by another piece 
 			while (config.getBoard()[i][j].equals("wP") || config.getBoard()[i][j].equals("wK")) {
 				System.out.print("That spot on the board is already occupied by one of your other pieces. Choose another location on the board. \n");
 				System.out.print("Pick a row to move your piece to: ");
@@ -107,7 +113,7 @@ public class Game {
 				}
 			}
 
-			//prompts user to choose a piece
+			//prompts player 2 to choose a piece
 			System.out.print("Player 2. Choose a piece: ");
 			String change2 = keyboard.next();
 			while(!change2.equals("bK") && !change2.equals("bP")){
@@ -115,6 +121,7 @@ public class Game {
 				change2 = keyboard.next();
 			}
 
+			//prompts player 2 to choose the row that their piece is on from 0-4
 			System.out.print("Which row is this piece on? ");
 			int rRemove2 = keyboard.nextInt();
     			while (rRemove2 > 4 || rRemove2 < 0) {
@@ -122,6 +129,7 @@ public class Game {
 				rRemove2 = keyboard.nextInt();
 			}
 
+			//prompts player 2 to choose the column that their piece is on from 0-4
 			System.out.print("Which column is this piece on? ");
 			int cRemove2 = keyboard.nextInt();
     			while (cRemove2 > 4 || cRemove2 < 0) {
@@ -129,6 +137,7 @@ public class Game {
 				cRemove2 = keyboard.nextInt();
 			}
 
+			//checks if the piece exists at the spot on the board chosen by player 2
 			while (!config.getBoard()[rRemove2][cRemove2].equals(change2)) {
 				System.out.print("That spot on the board does not have that piece. Pick a location where your piece exists. \n");
 				System.out.print("Which row is this piece on? ");
@@ -145,7 +154,7 @@ public class Game {
 				}
 			}
 			
-			//prompts user to move their piece to a row from 0-4
+			//prompts player 2 to move their piece to a row from 0-4
 			System.out.print("Pick a row to move your piece to: ");
 			int i2 = keyboard.nextInt();
     			while (i2 > 4 || i2 < 0) {
@@ -153,7 +162,7 @@ public class Game {
 				i2 = keyboard.nextInt();
 			}
 
-			//prompts user to move their piece to a column from 0-4
+			//prompts player 2 to move their piece to a column from 0-4
 			System.out.print("Pick a column to move your piece to: ");
 			int j2 = keyboard.nextInt();
 			System.out.print("\n");
@@ -163,6 +172,7 @@ public class Game {
 				System.out.print("\n");
 			}
 
+			//checks if the spot on the board that player 2 wants to move the piece to is already occupied by another piece 
 			while (config.getBoard()[i2][j2].equals("bP") || config.getBoard()[i2][j2].equals("bK")) {
 				System.out.print("That spot on the board is already occupied by one of your other pieces. Choose another location on the board. \n");
 				System.out.print("Pick a row to move your piece to: ");
@@ -180,16 +190,31 @@ public class Game {
 				}
 			}
 
-			//changes board to a new state according to what the user inputted
-			config.changeBoard("__", rRemove, cRemove);
-			config.changeBoard(change, i, j);
-			config.changeBoard("__", rRemove2, cRemove2);
-			config.changeBoard(change2, i2, j2);
-
-			//still need to get the computer inputs
+			//changes board to a new state according to what the two players inputted
+			if (i == i2 && j == j2) {
+				if (change.equals("wK") && change2.equals("bP")) {
+					config.changeBoard("__", rRemove, cRemove);
+					config.changeBoard("__", rRemove2, cRemove2);
+					config.changeBoard(change, i, j);
+				}
+				else if (change.equals("wP") && change2.equals("bK")) {
+					config.changeBoard("__", rRemove, cRemove);
+					config.changeBoard("__", rRemove2, cRemove2);
+					config.changeBoard(change2, i2, j2);
+				}
+				else if (change.equals("wP") && change2.equals("bP")) {
+					config.changeBoard("__", rRemove, cRemove);
+					config.changeBoard("__", rRemove2, cRemove2);
+				}
+			}
+			else {
+				config.changeBoard("__", rRemove, cRemove);
+				config.changeBoard(change, i, j);
+				config.changeBoard("__", rRemove2, cRemove2);
+				config.changeBoard(change2, i2, j2);
+			}
 
 			//seeing if either black or white won by checking the state of the board
-
   			for (int p = 0; p < 5; p++) {
     				for (int k = 0; k < 5; k++) {
 					list.add(config.getBoard()[p][k]);
@@ -202,6 +227,8 @@ public class Game {
 					}
 				}
 			}
+			
+			//counts how many White Pawns and Black Pawns are currently on the board
 			int w = 0;
 			int b = 0;
 			for (int z = 0; z < list.size(); z++) {
@@ -213,17 +240,17 @@ public class Game {
 				}
 			}
 
-			//if white has at least one pawn but black has none, then white won
+			//if white has at least one pawn but black has none, then white won and the loop ends
 			if (b == 0 && w > 0) {
 				System.out.print("White has won");
 				break;
 			}
-			//if black has at least one pawn but white has none, then black won
+			//if black has at least one pawn but white has none, then black won and the loop ends
 			else if (w == 0 && b > 0) {
 				System.out.print("Black has won");
 				break;
 			}
-			//if both black and white have no pawns, then it's a draw
+			//if both black and white have no pawns, then it's a draw and the loop ends
 			else if (w == 0 && b == 0) {
 				System.out.print("Draw.");
 				break;
